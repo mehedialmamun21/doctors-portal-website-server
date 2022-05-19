334e3727f1d6ea22ad0db0d623a2a8b815cb43cd
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-19 15:59:21 +0600

    admin making procedure done successfully

--- index.js
+++ index.js
@@ -51,6 +51,19 @@ async function run() {
             res.send(users);
         })
 
+
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
