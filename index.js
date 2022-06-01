2cffd69daacd734dba1f7e154b3baa843fb7f366
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-06-02 00:50:07 +0600

    final commit with all functionality working successfully in localhost 5000

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware //
-app.use(cors({ origin: 'https://doctors-portal-a0858.web.app' }));
+app.use(cors());
+// app.use(cors({ origin: 'https://doctors-portal-a0858.web.app' }));
 app.use(express.json());
 
 
@@ -255,5 +256,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-module.exports = app;
