1a490658abf3bc3af4b6441c1abf428527b8a40a
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-06-06 17:58:55 +0600

    extra commented portion deleted

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express()
 const port = process.env.PORT || 5000;
 
-// middleware //
+// middleware
 app.use(cors());
-// app.use(cors({ origin: 'https://doctors-portal-a0858.web.app' }));
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfepv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
