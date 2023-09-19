b78533e33413a63289c20546c2a3a72e8ea4d0a7
mehedialmamun21 <mehedialmamuncse@gmail.com>
2023-09-20 01:44:54 +0600

    delete item api of item management section added

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ async function run() {
             res.send(result);
         })
 
+        // Delete Menu Items
+        app.delete('/menu/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // Cart Collection
         app.get('/carts', async (req, res) => {
             const email = req.query.email;
